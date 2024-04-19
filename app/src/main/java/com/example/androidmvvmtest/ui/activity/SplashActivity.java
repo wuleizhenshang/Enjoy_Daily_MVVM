e0b57248b5661f3b2f004d1d9cc7b1810137ec57
wuleizhenshang <wuleizhenshang@163.com>
2024-04-19 20:20:27 +0800

    feat:MVVM架构完成功能、基本完善暗色适配和中英适配

--- app/src/main/java/com/example/androidmvvmtest/ui/activity/SplashActivity.java
+++ app/src/main/java/com/example/androidmvvmtest/ui/activity/SplashActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.KeyEvent;
 import android.view.View;
 
@@ -22,33 +23,12 @@ import com.example.androidmvvmtest.utils.MVUtils;
  */
 public class SplashActivity extends BaseActivity {
 
+    MVUtils mvUtils;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActivitySplashBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_splash);
-//        setStatusBar(true);//设置状态栏为深色
-        //根据是否登录进行跳转，这里就是模拟一下
-        EasyAnimation.moveViewWidth(binding.tvTranslate, new EasyAnimation.TranslateCallback() {
-            @Override
-            public void animationEnd() {
-                binding.tvMvvm.setVisibility(View.VISIBLE);
-                jumpActivityFinish(MVUtils.getBoolean(Constant.IS_LOGIN) ? HomeActivity.class : LoginActivity.class);
-            }
-        });
-    }
-
-    private long timeMillis;
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if (keyCode == KeyEvent.KEYCODE_BACK && event.getAction() == KeyEvent.ACTION_DOWN) {
-            if ((System.currentTimeMillis() - timeMillis) > 2000) {
-                showCustomMsg(context.getString(R.string.press_twice_to_exit),1500);
-                timeMillis = System.currentTimeMillis();
-            } else {
-                exitTheProgram();
-            }
-            return false;
-        }
-        return super.onKeyDown(keyCode, event);
+        setContentView(ActivitySplashBinding.inflate(getLayoutInflater()).getRoot());
+        new Handler().postDelayed(() -> jumpActivityFinish(mvUtils.getBoolean(Constant.IS_LOGIN) ? HomeActivity.class : LoginActivity.class),400);
     }
 }
